cf68920002b4874297f87536ce716d7e253af2a1
saran13 <saran.parv@gmail.com>
2016-09-01 15:32:01 +0530

    Create string.java

---
+++ string.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class string
+{
+public Static void main(String[] args)
+{
+string s="saranya";
+StrinfBuffer s1=new StringBuffer(s);
+a=s1.reverse.tostring();
+System.out.println(a);
+}
