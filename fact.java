5d71fa47f45e1867aca2cec3e3e9b63d6b77d00a
saran13 <saran.parv@gmail.com>
2016-09-01 16:12:21 +0530

    Update fact.java

--- fact.java
+++ fact.java
@@ -3,10 +3,13 @@ class fact
 {
 public static void main(String[] args)
 {
-fact=0;
-scanner s=new scanner(System.in);
-s=d.NextInt();
+fact=1,n,i,a;
+Scanner s=new Scanner(System.in);
+n=s.nextInt();
+for(i=0;i<=n;i++)
+{
 fact=fact*i;
+}
 system.out.println(fact);
 }
 }
