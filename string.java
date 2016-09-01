6c5878f2745aa79ef98822df33a4f94061b9eaf6
saran13 <saran.parv@gmail.com>
2016-09-01 15:54:22 +0530

    Update string.java

--- string.java
+++ string.java
@@ -1,11 +1,12 @@
 import java.util.*;
 class string
 {
-public Static void main(String[] args)
+public static void main(String[] args)
 {
-string s="saranya";
+  String a;
+String s="saranya";
 StrinfBuffer s1=new StringBuffer(s);
-a=s1.reverse.tostring();
+a=s1.reverse().tostring();
 System.out.println(a);
 }
 }
