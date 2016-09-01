c24530c2234381c62c1e4c48d0506be4ea0020b4
saran13 <saran.parv@gmail.com>
2016-09-01 15:59:12 +0530

    Create fact.java

---
+++ fact.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class fact
+{
+public static void main(String[] args)
+{
+fact=0;
+scanner s=new scanner(System.in);
+s=d.NextInt();
+fact=fact*i;
+system.out.println(fact);
+}
+}
